618a1e01889e28a29f13d46e1463c44f056309e6
Siberian <vasileios.kritikos@aueb.gr>
2025-03-22 15:32:03 +0200

    Product Collection

---
+++ ProductsCompareApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using ProductsCompareApp.Model;
+
+namespace ProductsCompareApp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        List<Product> products = [new Product() { Description = "Milk", Price = 12.5, Quantity = 1},
+                                    new Product() { Description = "Eggs", Price = 2.5, Quantity = 4},
+                                    new Product() { Description = "Oranges", Price = 8.99, Quantity = 85},
+                                    new Product() { Description = "Apples", Price = 4.99, Quantity = 40},
+                                  ]; 
+        products.Sort(); //sort implemented base on IComperable (Description)
+        products.ForEach(Console.WriteLine);
+        Console.WriteLine("------------");
+        
+        products.Sort((p2,p1) => p2.Price.CompareTo(p1.Price)); // sort implemented by Price DESC
+        products.Reverse();
+        products.ForEach(Console.WriteLine);        
+        Console.WriteLine("------------");
+        
+        products.Sort((p1,p2) => p1.Quantity - p2.Quantity); // sort implemented by Quantity ASC
+        products.ForEach(Console.WriteLine);
+        
+        Console.WriteLine("------------");
+    }
+}
